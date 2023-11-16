ce6d48f1171a8f7c42bc886dcbfe4ea984a30e23
Noalino <bgelineau.dev@gmail.com>
2023-11-16 17:37:10 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Noalino on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
